5c11dd201e819f836299c07f4b752172facaec5e
manahilmustaf09-ops <manahil.mustafa09@gmail.com>
2025-09-08 01:25:44 +0500

    Prepare for Vercel deployment

--- my-app/src/pages/RecipeDetail.jsx
+++ my-app/src/pages/RecipeDetail.jsx
@@ -1,14 +1,5 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import biryaniImg from '../public/images/biryani.jpg';
-import alferdoImg from '../public/images/alfredoPasta.jpg';
-import sandwichImg from '../public/images/sandwich.jpg';
-import pizzaImg from '../public/images/pizza.jpg';
-import karahiImg from '../public/images/karahi.jpg';
-import saladImg from '../public/images/salad.jpg';
-import butterImg from '../public/images/butter.jpg';
-import cakeImg from '../public/images/cake.jpg';
-import beefBurgerImg from '../public/images/beefBurger.jpg';
 
 
 
@@ -18,15 +9,15 @@ export default function RecipeDetail() {
 
   useEffect(() => {
     const sampleRecipes = [
-      { id: 1, title: "Chicken Biryani", rating: 4.8, image: biryaniImg , description: "A spicy rice dish with tender chicken, aromatic spices, and basmati rice." },
-      { id: 2, title: "Pasta Alfredo", rating: 4.5, image: alferdoImg, description: "Creamy pasta with parmesan, butter, and rich white sauce." },
-      { id: 3, title: "Grilled Sandwich", rating: 4.2, image: sandwichImg, description: "Crispy bread filled with cheese and veggies, grilled to perfection." },
-      { id: 4, title: "Margherita Pizza", rating: 4.7, image: pizzaImg, description: "Classic Italian pizza with tomato sauce, mozzarella, and basil." },
-      { id: 5, title: "Chicken Karahi", rating: 4.9, image: karahiImg, description: "Pakistani-style chicken curry cooked in a wok with spices." },
-      { id: 6, title: "Veggie Salad", rating: 4.1, image: saladImg, description: "Fresh salad with lettuce, cucumber, tomatoes, and dressing." },
-      { id: 7, title: "Butter Chicken", rating: 4.6, image: butterImg, description: "Rich curry made with chicken in creamy tomato gravy." },
-      { id: 8, title: "Chocolate Cake", rating: 4.9, image: cakeImg, description: "Moist chocolate cake topped with creamy chocolate frosting." },
-      { id: 9, title: "Beef Burger", rating: 4.4, image: beefBurgerImg, description: "Juicy beef patty with cheese, lettuce, and sauce in soft buns." }
+      { id: 1, title: "Chicken Biryani", rating: 4.8, image:"/images/biryani.jpg", description: "A spicy rice dish with tender chicken, aromatic spices, and basmati rice." },
+      { id: 2, title: "Pasta Alfredo", rating: 4.5, image: "/images/alfredoPasta.jpg", description: "Creamy pasta with parmesan, butter, and rich white sauce." },
+      { id: 3, title: "Grilled Sandwich", rating: 4.2, image: "/images/sandwich.jpg", description: "Crispy bread filled with cheese and veggies, grilled to perfection." },
+      { id: 4, title: "Margherita Pizza", rating: 4.7, image: "/images/pizza.jpg", description: "Classic Italian pizza with tomato sauce, mozzarella, and basil." },
+      { id: 5, title: "Chicken Karahi", rating: 4.9, image: "/images/karahi.jpg", description: "Pakistani-style chicken curry cooked in a wok with spices." },
+      { id: 6, title: "Veggie Salad", rating: 4.1, image: "/images/salad.jpg", description: "Fresh salad with lettuce, cucumber, tomatoes, and dressing." },
+      { id: 7, title: "Butter Chicken", rating: 4.6, image: "/images/butter.jpg", description: "Rich curry made with chicken in creamy tomato gravy." },
+      { id: 8, title: "Chocolate Cake", rating: 4.9, image: "/images/cake.jpg", description: "Moist chocolate cake topped with creamy chocolate frosting." },
+      { id: 9, title: "Beef Burger", rating: 4.4, image: "/images/beefBurger.jpg", description: "Juicy beef patty with cheese, lettuce, and sauce in soft buns." }
     ];
 
     const found = sampleRecipes.find((r) => r.id === parseInt(id));
